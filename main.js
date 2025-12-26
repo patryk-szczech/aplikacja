d621c269440f9b17c3c130f0def44e2651bf68da
patryk-szczech <123065037+patryk-szczech@users.noreply.github.com>
2025-12-26 11:22:38 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+  mainWindow.webContents.openDevTools();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
