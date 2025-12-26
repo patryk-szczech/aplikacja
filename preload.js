a7f9176081a2ee7c9b0b6383dec46f2b2e43b593
patryk-szczech <123065037+patryk-szczech@users.noreply.github.com>
2025-12-26 18:46:14 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,30 +1,54 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  getParents: () => ipcRenderer.invoke('get-parents'),
-  addParent: (parent) => ipcRenderer.invoke('add-parent', parent),
-  deleteParent: (id) => ipcRenderer.invoke('delete-parent', id),
-  
-  getChildren: () => ipcRenderer.invoke('get-children'),
-  addChild: (child) => ipcRenderer.invoke('add-child', child),
-  deleteChild: (id) => ipcRenderer.invoke('delete-child', id),
-  
-  getRates: () => ipcRenderer.invoke('get-rates'),
-  addRate: (rate) => ipcRenderer.invoke('add-rate', rate),
-  deleteRate: (id) => ipcRenderer.invoke('delete-rate', id),
-  
-  getPayments: () => ipcRenderer.invoke('get-payments'),
-  addPayment: (payment) => ipcRenderer.invoke('add-payment', payment),
-  
-  getAttendance: (month) => ipcRenderer.invoke('get-attendance', month),
-  setAttendance: (childId, date, present) => ipcRenderer.invoke('set-attendance', childId, date, present),
-  
-  sendEmail: (options) => ipcRenderer.invoke('send-email', options),
-  
-  backupData: (data) => ipcRenderer.invoke('backup-data', data),
-  restoreData: () => ipcRenderer.invoke('restore-data'),
-  
-  getEmailConfig: () => ipcRenderer.invoke('get-email-config'),
-  saveEmailConfig: (config) => ipcRenderer.invoke('save-email-config', config),
-  testEmail: (config) => ipcRenderer.invoke('test-email', config)
+  // Placówka
+  getPlacowka: () => ipcRenderer.invoke('getPlacowka'),
+  addPlacowka: (data) => ipcRenderer.invoke('addPlacowka', data),
+  updatePlacowka: (id, data) => ipcRenderer.invoke('updatePlacowka', id, data),
+
+  // Grupy
+  getGrupy: (placowkaId) => ipcRenderer.invoke('getGrupy', placowkaId),
+  addGrupa: (data) => ipcRenderer.invoke('addGrupa', data),
+  deleteGrupa: (id) => ipcRenderer.invoke('deleteGrupa', id),
+
+  // Rodzice
+  getRodzice: (placowkaId) => ipcRenderer.invoke('getRodzice', placowkaId),
+  addRodzic: (data) => ipcRenderer.invoke('addRodzic', data),
+  deleteRodzic: (id) => ipcRenderer.invoke('deleteRodzic', id),
+
+  // Dzieci
+  getDzieci: () => ipcRenderer.invoke('getDzieci'),
+  addDziecko: (data) => ipcRenderer.invoke('addDziecko', data),
+  deleteDziecko: (id) => ipcRenderer.invoke('deleteDziecko', id),
+
+  // Stawki
+  getStawki: () => ipcRenderer.invoke('getStawki'),
+  addStawka: (data) => ipcRenderer.invoke('addStawka', data),
+  deleteStawka: (id) => ipcRenderer.invoke('deleteStawka', id),
+
+  // Płatności
+  getPlatnosci: () => ipcRenderer.invoke('getPlatnosci'),
+  addPlatnosc: (data) => ipcRenderer.invoke('addPlatnosc', data),
+  deletePlatnosc: (id) => ipcRenderer.invoke('deletePlatnosc', id),
+
+  // Statystyki
+  getStatystyki: (grupaId) => ipcRenderer.invoke('getStatystyki', grupaId),
+
+  // Raporty
+  generatePdfReport: (type, options) => ipcRenderer.invoke('generatePdfReport', type, options),
+  generateExcelReport: (type, options) => ipcRenderer.invoke('generateExcelReport', type, options),
+  generateInvoice: (rodzicId, date) => ipcRenderer.invoke('generateInvoice', rodzicId, date),
+
+  // Import
+  selectImportFile: () => ipcRenderer.invoke('selectImportFile'),
+  importCSV: (filePath) => ipcRenderer.invoke('importCSV', filePath),
+  importXLSX: (filePath) => ipcRenderer.invoke('importXLSX', filePath),
+
+  // Backup
+  backupData: () => ipcRenderer.invoke('backupData'),
+  restoreData: () => ipcRenderer.invoke('restoreData'),
+
+  // Helpers
+  calculatePaymentFromAttendance: (dzieckoId, month) => ipcRenderer.invoke('calculatePaymentFromAttendance', dzieckoId, month),
+  getArrearsForParent: (rodzicId) => ipcRenderer.invoke('getArrearsForParent', rodzicId)
 });
